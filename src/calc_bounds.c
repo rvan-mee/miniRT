8d40eb568f8e4045be3d5ed33fdcda59cac588a5
Lucas Sinke <lsinke@student.codam.nl>
2022-11-06 20:54:27 +0100

    Add cyl bounds and manycyls map

--- src/calc_bounds.c
+++ src/calc_bounds.c
@@ -2,6 +2,31 @@
 #include <math.h>
 #include <stdlib.h>
 
+static t_aabb	cyl_bounds(t_object *obj)
+{
+	const t_aabb	bot = {
+			obj->coords - (obj->cylinder.diameter / 2),
+			obj->coords + (obj->cylinder.diameter / 2)
+	};
+	const t_aabb	top = {
+			obj->coords + obj->cylinder.top - (obj->cylinder.diameter / 2),
+			obj->coords + obj->cylinder.top + (obj->cylinder.diameter / 2)
+	};
+
+	return ((t_aabb) {
+		.min = {
+			fminf(bot.min[X], top.min[X]),
+			fminf(bot.min[Y], top.min[Y]),
+			fminf(bot.min[Z], top.min[Z]),
+		},
+		.max = {
+			fmaxf(bot.max[X], top.max[X]),
+			fmaxf(bot.max[Y], top.max[Y]),
+			fmaxf(bot.max[Z], top.max[Z]),
+		}
+	});
+}
+
 t_aabb	calc_bounds(t_object *obj)
 {
 	switch (obj->type) {
@@ -15,6 +40,8 @@ t_aabb	calc_bounds(t_object *obj)
 				{-10000, -10000, -10000},
 				{10000, 10000, 10000}
 			});
+		case CYLINDER:
+			return (cyl_bounds(obj));
 		default:
 			exit(2); // todo, i mean cyl n shit
 	}

cee1d435b55eba54fb9a5e77b09206239ad33a4b
Lucas Sinke <lsinke@student.codam.nl>
2023-03-19 01:59:31 +0100

    Rewrite parse_face to be norm compatible

--- src/parse/objects/parse_face.c
+++ src/parse/objects/parse_face.c
@@ -17,137 +17,6 @@
 #define B 1
 #define C 2
 
-typedef struct s_face_indices {
-	int32_t	vert_index;
-	bool	has_texture;
-	bool	has_normal;
-	int32_t	normal_index;
-	int32_t	vert_texture_index;
-}	t_face_indices;
-
-static bool	parse_normal(char **linep, t_face_indices *index, t_conf_data *conf)
-{
-	char	*line;
-	int32_t	new_index;
-
-	line = *linep;
-	if (*line != '/')
-		return (false);
-	line++;
-	if (!ft_isdigit(*line) && *line != '-')
-		return (false);
-	index->has_normal = true;
-	new_index = ft_atoi(line) - 1;
-	if (new_index < 0)
-		new_index += conf->v.length + 1;
-	if (new_index < 0 || (size_t)new_index > conf->vn.length)
-		return (false);
-	line++;
-	skip_digits(&line);
-	index->normal_index = new_index;
-	*linep = line;
-	return (true);
-}
-
-static bool	parse_vertex_texture(char **linep, \
-								t_face_indices *index, t_conf_data *conf)
-{
-	char	*line;
-	int32_t	new_index;
-
-	line = *linep;
-	if (!ft_isdigit(*line) && *line != '-')
-		return (false);
-	new_index = ft_atoi(line) - 1;
-	if (new_index < 0)
-		new_index += conf->v.length + 1;
-	if (new_index < 0 || (size_t)new_index > conf->vt.length)
-		return (false);
-	index->vert_texture_index = new_index;
-	line++;
-	skip_digits(&line);
-	if (*line == '/')
-		index->has_normal = true;
-	*linep = line;
-	return (true);
-}
-
-static bool	parse_vert_index(char **linep, \
-							t_face_indices *index, t_conf_data *conf)
-{
-	char	*line;
-	int32_t	new_index;
-
-	line = *linep;
-	skip_spaces(&line);
-	if (!ft_isdigit(*line) && *line != '-')
-		return (false);
-	new_index = ft_atoi(line) - 1;
-	if (new_index < 0)
-		new_index += conf->v.length + 1;
-	if (new_index < 0 || (size_t)new_index > conf->v.length)
-		return (false);
-	index->vert_index = new_index;
-	line++;
-	skip_digits(&line);
-	if (*line == '/')
-	{
-		line++;
-		if (ft_isdigit(*line) || *line == '-')
-			index->has_texture = true;
-		else if (*line == '/')
-			index->has_normal = true;
-		else
-			return (false);
-	}
-	*linep = line;
-	return (true);
-}
-
-static t_parse_err	parse_indices(
-	char **linep, \
-	t_face_indices *index, \
-	t_conf_data *conf, \
-	int32_t check_pre)
-{
-	const bool	pre_has_texture = index->has_texture;
-	const bool	pre_has_normal = index->has_normal;
-	char		*line;
-
-	line = *linep;
-	index->has_texture = false;
-	index->has_normal = false;
-	if (!parse_vert_index(&line, index, conf))
-		return (INDEX);
-	if (check_pre && index->has_texture != pre_has_texture)
-		return (MATCH);
-	if (index->has_texture && !parse_vertex_texture(&line, index, conf))
-		return (VERT_TEXTURE);
-	if (check_pre && index->has_normal != pre_has_normal)
-		return (MATCH);
-	if (index->has_normal && !parse_normal(&line, index, conf))
-		return (NORMAL);
-	*linep = line;
-	return (SUCCESS);
-}
-
-static void	set_indices(
-	t_object *object, \
-	t_conf_data *conf, \
-	t_face_indices *index, \
-	int32_t vert)
-{
-	const t_fvec	*normals = conf->vn.arr;
-	const t_fvec	*vertices = conf->v.arr;
-	const t_fvec	*texture = conf->vt.arr;
-
-	object->face.vert[vert] = vertices[index->vert_index];
-	if (index->has_normal)
-		object->face.normals[vert] = normals[index->normal_index];
-	if (index->has_texture)
-		object->face.uvw[vert] = texture[index->vert_texture_index];
-}
-
 static void	precalc(t_object *obj)
 {
 	const t_aabb	bounds = calc_bounds(obj);
@@ -159,29 +28,92 @@ static void	precalc(t_object *obj)
 	obj->coords = (bounds.min + bounds.max) / 2.f;
 }
 
-t_parse_err	parse_face(char **linep, t_object *object, t_conf_data *conf)
+static void	skip_int(char **linep)
 {
-	t_face_indices	indices;
-	t_parse_err	err;
-	char			*line;
-	int32_t			vert;
+	char	*line;
 
 	line = *linep;
+	if (*line == '-' || *line == '+')
+		++line;
+	while (ft_isdigit(*line))
+		++line;
+	*linep = line;
+}
+
+static const t_parse_err	g_errs[] = {VERT, VERT_TEXTURE, NORMAL};
+
+static t_parse_err	parse_is(char **linep, int32_t dst[3], const size_t len[3])
+{
+	uint8_t	i;
+
+	skip_spaces(linep);
+	i = 0;
+	while (true)
+	{
+		dst[i] = ft_atoi(*linep);
+		if (dst[i] < 0)
+			dst[i] += (int32_t) len[i];
+		if (dst[i] < 0 || dst[i] > (int32_t) len[i])
+			return (g_errs[i]);
+		skip_int(linep);
+		if (**linep == '\0' || ft_isspace(**linep))
+			break ;
+		if ((i < 2 && **linep != '/') || i == 2)
+			return (g_errs[i]);
+		(*linep) += 1;
+		++i;
+	}
+	return (SUCCESS);
+}
+
+static
+t_parse_err	set_idxs(t_face *face, int32_t idxs[3][3], t_conf_data *conf)
+{
+	const t_fvec	*v = conf->v.arr;
+	const t_fvec	*vt = conf->vt.arr;
+	const t_fvec	*vn = conf->vn.arr;
+	uint8_t			i;
+
+	if (idxs[0][0] == 0 || idxs[1][0] == 0 || idxs[2][0] == 0)
+		return (VERT);
+	face->has_texture = (idxs[0][1] || idxs[1][1] || idxs[2][1]);
+	if (face->has_texture && (!idxs[0][1] || !idxs[1][1] || !idxs[2][1]))
+		return (VERT_TEXTURE);
+	face->has_normal = (idxs[0][2] || idxs[1][2] || idxs[2][2]);
+	if (face->has_normal && (!idxs[0][2] || !idxs[1][2] || !idxs[2][2]))
+		return (NORMAL);
+	i = 3;
+	while (i--)
+	{
+		face->vert[i] = v[idxs[i][0] - 1];
+		if (face->has_texture)
+			face->uvw[i] = vt[idxs[i][1] - 1];
+		if (face->has_normal)
+			face->normals[i] = vn[idxs[i][2] - 1];
+	}
+	return (SUCCESS);
+}
+
+t_parse_err	parse_face(char **linep, t_object *object, t_conf_data *conf)
+{
+	const size_t	len[3] = {conf->v.length, conf->vt.length, conf->vn.length};
+	int32_t			idxs[3][3];
+	uint8_t			vert;
+	t_parse_err		err;
+
 	vert = A;
-	indices.has_normal = false;
-	indices.has_texture = false;
+	ft_bzero(idxs, sizeof(idxs));
 	object->colour = (t_fvec){1, 1, 1};
 	while (vert <= C)
 	{
-		err = parse_indices(&line, &indices, conf, vert);
+		err = parse_is(linep, idxs[vert++], len);
 		if (err != SUCCESS)
 			return (err);
-		set_indices(object, conf, &indices, vert);
-		vert++;
+		if (vert <= C && (**linep == '\0' || **linep == '\n'))
+			return (INDEX);
 	}
-	object->face.has_normal = indices.has_normal;
-	object->face.has_texture = indices.has_texture;
-	precalc(object);
-	*linep = line;
-	return (SUCCESS);
+	err = set_idxs(&object->face, idxs, conf);
+	if (err == SUCCESS)
+		precalc(object);
+	return (err);
 }
